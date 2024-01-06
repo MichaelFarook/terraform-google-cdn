aa7efa70dc17db48fac2526468371feadfe85fb7
MichaelFarook <michaeldegtiarev6995@gmail.com>
2024-01-06 21:44:19 +0200

    base of the module

---
+++ main.tf
@@ -0,0 +1,100 @@
+locals {
+  dns_name                = "${var.dns_cdn_domain}.${var.domain}"
+  backend_bucket_name     = "${var.prefix}-dashboard-bucket"
+  compute_url_map_name    = "${var.prefix}-url-map"
+  managed_ssl_name        = "${var.prefix}-managed-certificate"
+  target_https_proxy_name = "${var.prefix}-https-proxy"
+  global_address_name     = "${var.prefix}-public-address"
+  forwarding_rule_name    = "${var.prefix}-forwarding-rule"
+}
+
+##############################################################################
+# BACKEND GCS CDN BUCKET
+##############################################################################
+
+resource "google_compute_backend_bucket" "this" {
+  name        = local.backend_bucket_name
+  description = var.description
+  bucket_name = var.bucket_name
+  project     = var.project
+  enable_cdn  = true
+}
+
+##############################################################################
+# URL MAP
+##############################################################################
+
+resource "google_compute_url_map" "this" {
+  name            = local.compute_url_map_name
+  description     = var.description
+  default_service = google_compute_backend_bucket.this.self_link
+  project         = var.project
+}
+
+##############################################################################
+# MANAGED SSL/TLS CERT 
+##############################################################################
+
+resource "google_compute_managed_ssl_certificate" "this" {
+  name        = local.managed_ssl_name
+  description = var.description
+  project     = var.project
+  provider    = google-beta
+
+  managed {
+    domains = [local.dns_name]
+  }
+}
+
+##############################################################################
+# HTTPS PROXY
+##############################################################################
+
+resource "google_compute_target_https_proxy" "this" {
+  name             = local.target_https_proxy_name
+  description      = var.description
+  url_map          = google_compute_url_map.this.self_link
+  ssl_certificates = [google_compute_managed_ssl_certificate.this.self_link]
+  project          = var.project
+}
+
+##############################################################################
+# GLOBAL PUBLIC IP ADDRESS 
+##############################################################################
+
+resource "google_compute_global_address" "this" {
+  name         = local.global_address_name
+  description  = var.description
+  ip_version   = var.ip_version
+  address_type = var.address_type
+  project      = var.project
+}
+
+##############################################################################
+# GLOBAL FORWARDING RULE 
+##############################################################################
+
+resource "google_compute_global_forwarding_rule" "this" {
+  name        = local.forwarding_rule_name
+  description = var.description
+  target      = google_compute_target_https_proxy.this.self_link
+  ip_address  = google_compute_global_address.this.address
+  ip_protocol = var.ip_protocol
+  port_range  = var.port_range
+  project     = var.project
+}
+
+##############################################################################
+# create a dns record 
+##############################################################################
+
+resource "google_dns_record_set" "this" {
+  count = var.create_dns_record ? 1 : 0
+
+  managed_zone = var.dns_managed_zone
+  name         = local.dns_name
+  type         = var.dns_record_type
+  ttl          = var.dns_ttl
+  project      = var.project
+  rrdatas      = [google_compute_global_address.this.address]
+}
---
+++ varables.tf
@@ -0,0 +1,91 @@
+variable "project" {
+  description = "The ID of the project where this module will be created"
+  type        = string
+}
+
+variable "prefix" {
+  description = "Name prefix for the cdn resources"
+  type        = string
+  default     = "cdn"
+}
+
+variable "bucket_name" {
+  description = "The name of the bucket that will be the backend for our CDN"
+  type        = string
+}
+
+variable "domain" {
+  description = "The domain of your frontend application"
+  type        = string
+}
+
+variable "description" {
+  description = "An optional description of this resource"
+  type        = string
+  default     = ""
+}
+##############################################################################
+# GLOBAL PUBLIC IP ADDRESS
+##############################################################################
+
+variable "ip_version" {
+  description = "The IP version of cdn public address"
+  type        = string
+  default     = "IPV4"
+}
+
+variable "address_type" {
+  description = "The address type of cdn public address"
+  type        = string
+  default     = "EXTERNAL"
+}
+
+##############################################################################
+# GLOBAL FORWARDING RULE
+##############################################################################
+
+variable "ip_protocol" {
+  description = "The type of protocole to global forwarding rule"
+  type        = string
+  default     = "TCP"
+}
+
+variable "port_range" {
+  description = "The port range to global forwarding rule"
+  type        = string
+  default     = "443"
+}
+
+##############################################################################
+# DNS RECORD VARIABLES
+##############################################################################
+
+variable "create_dns_record" {
+  description = "Create the DNS record set to the frontend"
+  type        = bool
+  default     = false
+}
+
+variable "dns_managed_zone" {
+  description = "The managed zone of DNS"
+  type        = string
+  default     = ""
+}
+
+variable "dns_record_type" {
+  description = "The type of DNS record"
+  type        = string
+  default     = "A"
+}
+
+variable "dns_ttl" {
+  description = "Time to Live (TTL) for DNS record"
+  type        = number
+  default     = 300
+}
+
+variable "dns_cdn_domain" {
+  description = "Time CDN dashboard Domain"
+  type        = string
+  default     = ""
+}
